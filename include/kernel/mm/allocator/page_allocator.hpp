ec304c042fe95bf5710af9bcb3e6baa9327b14ed
mirika <59782711+MirikaRyu@users.noreply.github.com>
2025-05-22 22:34:57 +0800

    small fix for page allocator

--- include/kernel/mm/allocator/page_allocator.hpp
+++ include/kernel/mm/allocator/page_allocator.hpp
@@ -21,57 +21,59 @@ namespace kernel::mem
 
         free_block_t *next_;
 
-    public:
         using va_t = arch_traits<current_arch>::va_t<>;
+
+    public:
+        using pa_t = arch_traits<current_arch>::pa_t<>;
 
         static constexpr auto &page_size = arch_traits<current_arch>::page_size;
 
     public:
-        page_allocator(va_t start, va_t end) noexcept
+        page_allocator(pa_t start, pa_t end) noexcept
         {
             kassert(start && end, "initializing page allocator with null pointer is invalid");
             kassert(start.is_align_by(page_size) && end.is_align_by(page_size), "managed memory boundary misaligned");
-            kassert(end.address() > start.address(), "invalid memory range");
+            kassert(end.get() > start.get(), "invalid memory range");
 
-            auto diff_pages = (end.address() - start.address()) % page_size;
-            next_ = ::new (start) free_block_t{.page_count = diff_pages, .next = nullptr};
+            auto diff_pages = (end.get() - start.get()) % page_size;
+            next_ = ::new (va_t{start}) free_block_t{.page_count = diff_pages, .next = nullptr};
         }
 
-        [[nodiscard]] va_t alloc_page(void) noexcept
+        [[nodiscard]] pa_t alloc_page(void) noexcept
         {
             return alloc_page(1);
         }
 
-        [[nodiscard]] va_t alloc_page(size_t page_counts) noexcept
+        [[nodiscard]] pa_t alloc_page(size_t page_counts) noexcept
         {
-            // Fix me: use optional<va_t>
+            // Fix me: use optional<pa_t>
             if (next_ == nullptr || page_counts == 0)
-                return va_t{nullptr};
+                return pa_t{va_t{nullptr}};
 
             // find the first valid block
             free_block_t **visit_p = &next_;
             for (; (*visit_p)->page_count < page_counts; visit_p = &((*visit_p)->next))
                 if ((*visit_p)->next == nullptr) [[unlikely]]
-                    return va_t{nullptr};
+                    return pa_t{va_t{nullptr}};
 
             if ((*visit_p)->page_count == page_counts)
-                return va_t{std::exchange(*visit_p, (*visit_p)->next)};
+                return pa_t{va_t{std::exchange(*visit_p, (*visit_p)->next)}};
             else
-                return va_t{std::exchange(
+                return pa_t{va_t{std::exchange(
                     *visit_p,
                     ::new (*visit_p + page_counts * (page_size / sizeof(free_block_t)))
-                        free_block_t{.page_count = (*visit_p)->page_count - page_counts, .next = (*visit_p)->next})};
+                        free_block_t{.page_count = (*visit_p)->page_count - page_counts, .next = (*visit_p)->next})}};
         }
 
-        void dealloc_page(va_t pages, size_t counts = 1) noexcept
+        void dealloc_page(pa_t pages, size_t counts = 1) noexcept
         {
             kassert(pages.is_align_by(page_size), "try free misaligned memory address");
 
-            if (pages == nullptr || counts == 0)
+            if (!pages || counts == 0)
                 return;
 
             // This will cause a countinuous physical memory being splitted
-            next_ = ::new (pages) free_block_t{.page_count = counts, .next = next_};
+            next_ = ::new (va_t{pages}) free_block_t{.page_count = counts, .next = next_};
         }
 
         ~page_allocator() noexcept
